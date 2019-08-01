58fccd3a91cae1e582abd0170243ab562080a680
Winson Wei <westoy@gmail.com>
2019-08-02 00:03:27 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+@font-face {
+	font-family: 'GoodSansThin';
+	src: url('http://brian-baker.com/fonts/GoodSans-Thin.eot');
+	src: url('http://brian-baker.com/fonts/GoodSans-Thin.eot?#iefix') format('embedded-opentype'), 
+		 url('http://brian-baker.com/fonts/GoodSans-Thin.woff2') format('woff2'), 
+		 url('http://brian-baker.com/fonts/GoodSans-Thin.woff') format('woff');
+	font-weight: normal;
+	font-style: normal;
+}
+
+@font-face {
+	font-family: 'GoodSansLight';
+	src: url('http://brian-baker.com/fonts/GoodSans-Light.eot');
+	src: url('http://brian-baker.com/fonts/GoodSans-Light.eot?#iefix') format('embedded-opentype'), 
+		 url('http://brian-baker.com/fonts/GoodSans-Light.woff2') format('woff2'), 
+		 url('http://brian-baker.com/fonts/GoodSans-Light.woff') format('woff');
+	font-weight: normal;
+	font-style: normal;
+}
